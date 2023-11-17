6edd6746770de4cef5290bdcb7420d45a7b007c9
Gresdian <chris_da98@outlook.it>
2023-11-17 13:06:07 +0100

    exercise finished

--- js/script.js
+++ js/script.js
@@ -3,3 +3,15 @@ console.log(number);
 
 const pi = 3.14
 console.log(pi);
+
+/* pi = 3.1416
+console.log(pi); */
+
+let radius = 8;
+
+let circle = radius * 2 * pi;
+console.log(circle);
+
+let name = 'Carlo';
+name = 'Marco';
+console.log(name);
